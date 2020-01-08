223a1a7e6ebdea8db816047a52136050d6731af0
V tussenvoegsels Achternaam <jurgenvanderheiden@gmail.com>
2020-01-08 16:02:01 +0100

    Change folder structure

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
+import os
 from flask import Flask, render_template, url_for, request, redirect
 
-app = Flask(__name__)
+app = Flask(__name__,root_path=os.path.join(os.getcwd(), 'jeugdfonds'))
 
 @app.route('/')
 def index():
