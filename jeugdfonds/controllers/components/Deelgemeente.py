2777acbe2fe764e455803412e378903a42fce4a8
abvkilic <abdulvahidkilic@gmail.com>
2020-01-10 14:21:10 +0100

    UPDATE: added table

--- jeugdfonds/controllers/components/Deelgemeente.py
+++ jeugdfonds/controllers/components/Deelgemeente.py
@@ -2,10 +2,10 @@ class Deelgemeente:
     areaName = ""
     zipCodeRange = tuple()
     applicants = 0
-    intermediair = 0 
+    intermediary = 0 
 
-    def __init__(self,areaName : str, zipCodeRange : tuple, applicants : int, intermediair : int):
+    def __init__(self,areaName : str, zipCodeRange : tuple, applicants : int, intermediary : int):
         self.areaName = areaName
         self.zipCodeRange = zipCodeRange
         self.applicants = applicants
-        self.intermediair = intermediair
+        self.intermediary = intermediary
