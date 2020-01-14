3be7af659ca20bb1cf8a731736abb6179ca28b07
V tussenvoegsels Achternaam <jurgenvanderheiden@gmail.com>
2020-01-14 15:28:38 +0100

    Update

--- jeugdfonds/controllers/components/Applicant.py
+++ jeugdfonds/controllers/components/Applicant.py
@@ -7,50 +7,37 @@ class Applicant:
     DateOfBirth = ""
 
     def SetValues(self,firstName : str,affix : str,lastname : str,gender :str,zipCode : int, dateOfBirth):
-        if self.AreValuesValid(firstName,affix,lastname,gender,zipCode,dateOfBirth):
-            self.FirstName = firstName
-            self.Affix = affix
-            self.LastName = lastname
-            self.Gender = gender
-            self.ZipCode = zipCode
-            self.DateOfBirth = dateOfBirth
-    
-    def AreValuesValid(self,firstName : str,affix : str,lastname : str,gender :str,zipCode : int, dateOfBirth):
-        errors = []
-        errors.append(self.ValidateName(firstName))
+        self.FirstName = self.FilterName(firstName)
         if affix == "":
-            errors.append(self.ValidateName(affix))
-        errors.append(self.ValidateName(lastname))
-        errors.append(self.ValidateGender(gender))
-        errors.append(self.ValidateZipCode(zipCode))
-        errors.append(self.ValidateDateOfBirth(dateOfBirth))
-        if False in errors:
-            return False
-        return True
+            self.Affix = self.FilterName(affix)
+        self.LastName = self.FilterName(lastname)
+        self.Gender = self.FilterGender(gender)
+        self.ZipCode = self.FilterZipCode(zipCode)
+        self.DateOfBirth = self.FilterDateOfBirth(dateOfBirth)
 
-    def ValidateName(self,name):
+    def FilterName(self,name):
         if(name.replace(" ", "").isalpha()):
-            return True
-        return False
+            return name
+        return None
     
-    def ValidateGender(self,gender):
+    def FilterGender(self,gender):
         if gender == "Man" or gender == "Vrouw":
-            return True
-        return False
+            return gender
+        return None
     
-    def ValidateZipCode(self):
-        if (isinstance(zipCode),int):
-            return True
-        return False
+    def FilterZipCode(self,zipCode):
+        if type(zipCode) == int:
+            return zipCode
+        return None
     
-    def ValidateDateOfBirth(self,birthDate):
+    def FilterDateOfBirth(self,birthDate):
         try:
             day,month,year = birthDate.split('-')
             nBirthDate = datetime.datetime(int(year),int(month),int(day))
-            if (nBirthDate.year > datetime.datetime.now().year - int(10)):
-                return True
+            if (nBirthDate.year > datetime.datetime.now().year - int(18)):
+                return birthDate
             else:
-                return False
+                return None
         except:
-            return False
+            return None
         
